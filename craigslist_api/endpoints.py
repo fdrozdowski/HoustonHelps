f0b63b207ed30ffe1e16a1ca677e212ec8ee2f5a
Filip Drozdowski <fgd1@rice.edu>
2017-05-21 09:50:21 -0500

    Improve error handling

--- craigslist_api/endpoints.py
+++ craigslist_api/endpoints.py
@@ -1,4 +1,6 @@
 from flask import jsonify, Flask
+
+from craigslist_api.category_mapping import mapping
 from craigslist_api.search import get_all_free_items_in_category, get_item_in_category
 from craigslist_api.error import InvalidUsage
 
@@ -16,8 +18,11 @@ def index():
 @app.route('/items/<category>/<item>', methods=['GET'])
 def get_craigslist_category_data(category, item=None):
     if category is None and item is None:
-        return InvalidUsage('No category or item specified.', status_code=400)
+        raise InvalidUsage('No category or item specified.', status_code=400)
     elif item is None:
+        if category not in mapping:
+            raise InvalidUsage(category + ' is not a valid category. These are valid categories: ' +
+                                str(mapping.keys()), status_code=400)
         result = get_all_free_items_in_category(DEFAULT_CRAIGSLIST_SITE, category)
         return jsonify(result)
     else:
