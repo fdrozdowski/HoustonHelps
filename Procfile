960821d19fd1a47c9c669db7c10253d120938ce8
Filip Drozdowski <fgd1@rice.edu>
2017-05-21 00:42:27 -0500

    fix heroic profile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python3 run.py 0.0.0.0
+web: gunicorn run:application
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from craigslist_api.endpoints import app
 
+application = app
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
